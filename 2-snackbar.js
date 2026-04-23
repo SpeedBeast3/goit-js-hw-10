887375299af2167985c5927d13e200122f6db44c
SpeedBeast3 <zombozo223@gmail.com>
2026-04-23 11:42:24 +0000

    Deploying to gh-pages from @ SpeedBeast3/goit-js-hw-10@f11ff594ba7520607557a20b8888adac9c4d48b5 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{i}from"./assets/vendor-BbbuE1sJ.js";const r=document.querySelector(".form");r.addEventListener("submit",n);function n(t){t.preventDefault();const s=+t.target.elements.delay.value,o=t.target.elements.state.value;new Promise((e,m)=>{setTimeout(()=>{o==="fulfilled"?e(s):m(s)},s)}).then(e=>{i.success({message:`Fulfilled promise in ${e}ms`,position:"topRight"})}).catch(e=>{i.error({message:`Rejected promise in ${e}ms`,position:"topRight"})})}
+//# sourceMappingURL=2-snackbar.js.map
